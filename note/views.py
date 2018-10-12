c4493cdeabd65365c83cc14e4f15826b558d1569
toha-livs <anton.olivson@gmail.com>
2018-10-12 11:40:24 +0300

    adsss

--- note/views.py
+++ note/views.py
@@ -35,7 +35,7 @@ def snippet_list(request, format=None):
 
     elif request.method == 'POST':
         text = request.POST.get('body')
-        print(text)
+        print(request.POST)
         confirm_text = Posts(body=text)
         confirm_text.save()
         context = {'status': 'success', 'text': 'Your message is confirm!'}
